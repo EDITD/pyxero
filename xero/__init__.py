6b9c9ce2825cb67b4a38a7b4f18865a8648c961a
Panos Katseas <panos@editd.com>
2014-05-06 10:34:11 +0100

    Bump version

--- xero/__init__.py
+++ xero/__init__.py
@@ -2,9 +2,9 @@ try:
     # This try-catch is necessary to make sure we can get
     # VERSION from the base xero module before all the
     # dependencies have been imported.
-    from .api import Xero
+    from .api import Xero  # noqa
 except ImportError:
     pass
 
-NUM_VERSION = (0, 5, 2)
+NUM_VERSION = (0, 5, 3)
 VERSION = ".".join(str(nv) for nv in NUM_VERSION)
